363bdb465c6b87cbac56ae1ed8154f8fe2398f86
halop <halop8@gmail.com>
2018-09-11 05:49:44 +0200

    Update masternode.cpp

--- src/masternode.cpp
+++ src/masternode.cpp
@@ -324,8 +324,10 @@ void CMasternode::UpdateLastPaid(const CBlockIndex *pindex, int nMaxBlocksToScan
 
             CAmount nMasternodePayment = GetMasternodePayment(BlockReading->nHeight, block.vtx[0]->GetValueOut());
 
-            for (CTxOut txout : block.vtx[0]->vout)
-                if(mnpayee == txout.scriptPubKey && nMasternodePayment == txout.nValue) {
+            for (CTxOut txout : block.vtx[0]->vout) {
+                CTxDestination dest;
+                ExtractDestination(txout.scriptPubKey, dest);
+                if(EncodeDestination(mnpayee) == EncodeDestination(dest) && nMasternodePayment == txout.nValue) {
                     nBlockLastPaid = BlockReading->nHeight;
                     nTimeLastPaid = BlockReading->nTime;
                     LogPrint(MCLog::MN, "CMasternode::UpdateLastPaidBlock -- searching for block with payment to %s -- found new %d\n", vin.prevout.ToStringShort(), nBlockLastPaid);
